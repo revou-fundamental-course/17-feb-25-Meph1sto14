2766b7ecf9e93c535d31daccf59db75b64d672bb
Meph1sto14 <marvelsanjaya7@gmail.com>
2025-02-24 23:10:27 +0700

    ubah bagian navbar menjadi underline on hover

--- js/script.js
+++ js/script.js
@@ -1,10 +1,2 @@
 // Ini JavaScript Exsternal
 
-const navLinks = document.querySelectorAll('nav a');
-
-navLinks.forEach(link => {
-    link.addEventListener('click', (e) => {
-        navLinks.forEach(l => l.classList.remove('active'));
-        link.classList.add('active');
-    });
-});
